889b224d734575cf157a77e27a21fa6502d76db8
Ilya Kazlou <ilya02k@gmail.com>
2024-05-21 13:53:14 +0300

    swift ui 15

---
+++ swift ui 15/swift ui 15/ProductItemView.swift
@@ -0,0 +1,8 @@
+//
+//  ProductItemView.swift
+//  swift ui 15
+//
+//  Created by Ann Dosova on 21.05.24.
+//
+
+import Foundation
---
+++ swift ui 15/swift ui 15/swift_ui_15App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_15App.swift
+//  swift ui 15
+//
+//  Created by Ann Dosova on 21.05.24.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
